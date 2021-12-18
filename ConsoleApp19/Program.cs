6e701b9e28c86c05862d3f9b4e2a1ca425058818
StalerG <zyck83@mail.ru>
2021-12-19 01:12:14 +1000

    StalerG/ASCIIAquarium

--- ConsoleApp19/Program.cs
+++ ConsoleApp19/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using System.Threading;
+using static Aquarium.DateTimer;
+using static Aquarium.Program.FishAquarium;
+using static Aquarium.RandomGrass;
 using static System.Console;
 using static System.ConsoleColor;
 
@@ -8,103 +9,79 @@ namespace Aquarium
 {
     internal class Program
     {
-        public static class FishAquarium
+        public class FishAquarium
         {
-            private static readonly byte ConsoleWidthSize = 120;
-            private static readonly byte ConsoleHeightSize = 30;
-            private static readonly ConsoleColor WaterColor = Cyan;
-            private static readonly ConsoleColor SeaWeedColor = Green;
-            [DllImport(dllName:"kernel32.dll", ExactSpelling = true)]
-            private static extern IntPtr GetConsoleWindow();
-            [DllImport(dllName:"user32.dll", CharSet = CharSet.Auto)]
-            private static extern bool EnableScrollBar(IntPtr hWnd, int wSBflags, int wArrows);
-            public static void Console_ScrollBar_Settings()
+            private static int counter;
+
+            public FishAquarium()
             {
-                SetWindowSize(width: ConsoleWidthSize, height: ConsoleHeightSize);
-                IntPtr handle = GetConsoleWindow();
-                EnableScrollBar(handle, wSBflags:3, wArrows:3);
+                counter = 0;
             }
-            public static void Water(byte left = 0, byte top = 5)
+            private static readonly ConsoleColor IBlue = Blue;
+            private static readonly ConsoleColor IGreen = Green;
+            public static void Water(int left = 0, int top = 5)
             {
-                SetCursorPosition(left, top);
+                SetCursorPosition(left: left, top: top);
+                ForegroundColor = IBlue;
 
-                ForegroundColor = WaterColor;
+                int[] sea = new int[120];
+                for (int i = 0; i < sea.Length; i++) Write(value: "~");
 
-                byte count = 0;
-                byte[] str1 = new byte[120];
                 string[] str2 = { "^^^^ ^^^  ^^^   ^^^ ^^^^" };
                 string[] str3 = { "^^^^     ^^^^    ^^^  ^^" };
                 string[] str4 = { "^^   ^^^^   ^^^   ^^^^^^" };
 
-                for (byte i = 0; i < str1.Length; i++) Write("~");
-
-                while (count < 5)
+                while (counter < 5)
                 {
-                    count++;
-                    foreach (string item1 in str2) Write($"{item1}"); 
-                    foreach (string item2 in str3) Write($"{item2}"); 
+                    counter++;
+                    foreach (string item1 in str2) Write($"{item1}");
+                    foreach (string item2 in str3) Write($"{item2}");
                     foreach (string item3 in str4) Write($"{item3}");
                 }
                 ResetColor();
             }
-            public static void Castle(byte left = 15, byte top = 16)
+            public static void Castle(byte left = 0, byte top = 14)
             {
-                string flag = "~~";
-                string iCastle = @$"           
-                                                                                            T{flag}
-                                                                                            |
-                                                                                           /^\
-                                                                                          /   \
-                                                                              _   _   _  /     \  _   _   _
-                                                                             [ ]_[ ]_[ ]/ _   _ \[ ]_[ ]_[ ]
-                                                                             |_=__-_ =_|_[ ]_[ ]_|_=-___-__|
-                                                                              | _- =  | =_ = _    |= _=   |
-                                                                              |= -[]  |- = _ =    |_-=_[] |
-                                                                              | =_    |= - ___    | =_ =  |
-                                                                              |=  []- |-  /| |\   |=_ =[] |
-                                                                              |- =_   | =| | | |  |- = -  |
-                                                                              |_______|__|_|_|_|__|_______|";
-                int posY = ConsoleWidthSize - left;
-                int posX = ConsoleHeightSize - top;
-                SetCursorPosition(posY, posX);
-                Write($"{iCastle}");
-            }
-            public static void SeaWeed()
-            {
-                for (int i = 0; ; i++)
+                string[] iCastle = {
+                    $@"
+                                                                                         T~~
+                                                                                         |
+                                                                                        /^\
+                                                                                       /   \
+                                                                           _   _   _  /     \  _   _   _
+                                                                          [ ]_[ ]_[ ]/ _   _ \[ ]_[ ]_[ ]
+                                                                          |_=__-_ =_|_[ ]_[ ]_|_=-___-__|
+                                                                           | _- =  | =_ = _    |= _=   |
+                                                                           |= -[]  |- = _ =    |_-=_[] |
+                                                                           | =_    |= - ___    | =_ =  |
+                                                                           |=  []- |-  /| |\   |=_ =[] |
+                                                                           |- =_   | =| | | |  |- = -  |
+                                                                           |_______|__|_|_|_|__|_______|" };
+
+                foreach (var item in iCastle)
                 {
-                    ForegroundColor = SeaWeedColor;
-                    CursorVisible = false;
-                    char[] seaWeed = { '(', ')' };
-                    foreach (char item in seaWeed)
-                    {
-                        Thread.Sleep(100);
-                        SetCursorPosition(5, 27);
-                        Write(item);
-                        Thread.Sleep(50);
-                        SetCursorPosition(5, 26);
-                        Write(item);
-                        Thread.Sleep(100);
-                        SetCursorPosition(5, 25);
-                        Write(item);
-                        Thread.Sleep(50);
-                        SetCursorPosition(5, 24);
-                        Write(item);
-                        Thread.Sleep(100);
-                        SetCursorPosition(5, 23);
-                        Write(item);
-                    }
+                    SetCursorPosition(left: left, top: top);
+                    Write($"{item}");
                 }
+                ResetColor();
+            }
+            public static void SeaGrass()
+            {
+                ForegroundColor = IGreen;
+                string[] grass_img = { "( ", "   )", "  (  ", "   )" };
+                Grass(grass_img, left: 2, top: 24, speed: 75);
             }
         }
         static void Main()
         {
-            FishAquarium.Console_ScrollBar_Settings();
-            FishAquarium.Water();
-            FishAquarium.Castle();
-            FishAquarium.SeaWeed();
-            DateTimer.TimerDate();
-            ReadKey(intercept:false);
+            Water();
+            Castle();
+
+            while (true)
+            {
+                SeaGrass();
+                TimerDate(1, 28);
+            }
         }
     }
 }
---
+++ ConsoleApp19/RandomGrass.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using static System.Console;
+
+namespace Aquarium
+{
+    class RandomGrass
+    {
+        public static void Grass(string[] array, int left, int top, int speed)
+        {
+            SetCursorPosition(left, top);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            { 
+                WriteLine($"{array[i]}");
+            }
+            Speed(speed);
+        }
+        public static void Speed(int speed)
+        {
+            Thread.Sleep(speed);
+        }
+    }
+}
+
